aced4a9118da4b8ea473ea3bccbd3e12a0ee4f2b
akitary <akitary.commissions@gmail.com>
2021-08-04 18:27:35 +0200

    Purge

--- src/main/java/io/github/akiart/fantasia/common/enchantment/FEnchantmentType.java
+++
@@ -1,8 +0,0 @@
-package io.github.akiart.fantasia.common.enchantment;
-
-import io.github.akiart.fantasia.common.item.itemType.JavelinItem;
-import net.minecraft.enchantment.EnchantmentType;
-
-public class FEnchantmentType {
-    EnchantmentType JAVELIN = EnchantmentType.create("javelin", item -> item instanceof JavelinItem);
-}
